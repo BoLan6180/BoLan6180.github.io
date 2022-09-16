f0a3fb050ce9d81fc02111d93dfaac8ee3784308
Bo Lan <blan@trip.com>
2022-09-16 14:13:15 +0800

    feat: update lingjie rules config

--- lingjie-demo/lingjie-shell/shell-config.js
+++ lingjie-demo/lingjie-shell/shell-config.js
@@ -2,16 +2,16 @@ window.__lingjie_rules__ = [
   {
     test: "/project-a",
     timeout: 5000,
-    waitFor:'#components-layout-demo-custom-trigger'
+    waitFor:"loaded",
   },
   {
     test: "/project-b",
     timeout: 5000,
-    waitFor:'#components-layout-demo-custom-trigger'
+    waitFor:"loaded",
   },
   {
     test: "/project-c",
     timeout: 5000,
-    waitFor:'#components-layout-demo-custom-trigger'
-  }
+    waitFor:"loaded",
+  },
 ];
